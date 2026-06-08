99a24e9bbc5b4f1a8a2507cab43870841528cef3
Vilson2930 <vilsonjosepereirapinto@gmail.com>
2026-06-07 22:45:06 -0300

    Update __init__.py

--- engine/__init__.py
+++ engine/__init__.py
@@ -1,1 +1,12 @@
+# ============================================================
+# ORÇAMENTO INTELIGENTE
+# ENGINE PACKAGE
+# ============================================================
 
+from .pdf_engine import *
+from .transaction_engine import *
+from .merchant_engine import *
+from .category_engine import *
+from .parcelamento_engine import *
+from .diagnostico_engine import *
+from .recommendation_engine import *
