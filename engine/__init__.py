c87b70639d2fa8e220a44b4c1cff329144dc9885
Vilson2930 <vilsonjosepereirapinto@gmail.com>
2026-06-08 21:52:07 -0300

    Update __init__.py

--- engine/__init__.py
+++ engine/__init__.py
@@ -7,9 +7,10 @@ from .pdf_engine import *
 from .transaction_engine import *
 from .merchant_engine import *
 from .category_engine import *
+from .category_engine import *
 from .parcelamento_engine import *
 from .diagnostico_engine import *
 from .recommendation_engine import *
 from .compromissos_engine import *
 from .debt_engine import *
-from .ROTATIVO_ENGINE import *
+from .rotativo_engine import *
